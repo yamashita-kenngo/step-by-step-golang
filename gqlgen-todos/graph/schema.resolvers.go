32d90cef62f834ba7d025707c7bf63f0183e35f9
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2022-06-04 16:41:30 +0900

    feat: todos READ and CREATE add func create new todo and func read todos memo: TODOアイテム追加とTODOアイテムの読み取り

--- gqlgen-todos/graph/schema.resolvers.go
+++ gqlgen-todos/graph/schema.resolvers.go
@@ -8,14 +8,21 @@ import (
 	"fmt"
 	"gqlgen-todos/graph/generated"
 	"gqlgen-todos/graph/model"
+	"math/rand"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user" + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
